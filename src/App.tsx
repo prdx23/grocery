a1dc54243a6fda68fe5aa842519f3ff02b20adc9
Arsh <hi@prdx.me>
2023-05-04 09:50:10 +0530

    solid.js base setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import type { Component } from 'solid-js';
+
+import styles from './App.module.css';
+
+
+const App: Component = () => {
+    return (
+    <main class={styles.App}>
+        <p> hello! </p>
+    </main>
+    );
+};
+
+
+export default App;
